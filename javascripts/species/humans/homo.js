565c31ff91488932900dcbed4bde25d4abd5460c
Tim Creasy <timcreasy@me.com>
2016-07-05 15:48:57 -0500

    Added species

---
+++ javascripts/species/humans/homo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var Human = require("../../Human");
+
+var Homo = function (name) {
+  this.name = name;
+  this.habitat = "water";
+  this.species = "Homo Sapien";
+};
+
+Homo.prototype = new Human();
+
+module.exports = Homo;
