686b1341e803cb9c0199bd81f8efb3fb5193d362
Tim Creasy <timcreasy@me.com>
2016-07-05 14:51:03 -0500

    Created prototype of Player, Human, and Dinosaur

---
+++ javascripts/player.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var Player = function () {
+  this.class = null;
+  this.species = null;
+  this.health = 100;
+  this.attack = 10;
+  this.defense = 10;
+  this.attackType = null;
+  this.habitat = null;
+  this.name = null;
+};
+
+module.exports = Player;
