8fbc4565388e1479ae9a5d9ccfd5edd6293bd7cc
Tim Creasy <timcreasy@me.com>
2016-07-08 12:59:23 -0500

    Added multiple weapons

--- javascripts/Human.js
+++ javascripts/Human.js
@@ -5,16 +5,36 @@ var Player = require("./Player");
 var Human = function () {
   this.class = "Human";
   this.intelligience = null;
-  this.attackType = "punch";
   this.weapon = null;
   this.healthMin = 85;
   this.healthMax = 125;
+  this.attackType = [
+    {
+      attack: "punch",
+      attackMin: 7,
+      attackMax: 12
+    },
+    {
+      attack: "eye gouge",
+      attackMin: 5,
+      attackMax: 9
+    },
+    {
+      attack: "knife",
+      attackMin: 8,
+      attackMax: 10
+    },
+    {
+      attack: "choke",
+      attackMin: 6,
+      attackMax: 10
+    }
+
+  ];
   this.attackMin = 7;
   this.attackMax = 12;
   this.health = Math.floor(Math.random() * (this.healthMax - this.healthMin + 1)) + this.healthMin;
-  this.attack = function() {
-    return Math.floor(Math.random() * (this.attackMax - this.attackMin + 1)) + this.attackMin;
-  };
+
 };
 
 Human.prototype = new Player();
